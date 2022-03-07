6dc960fae96ec6d6347fb7e9a2e34a3548280aa8
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-03-06 20:51:42 -0500

    3

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ejemplotoolbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
@@ -18,6 +20,13 @@ class MainActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
         toolbar?.setTitle(R.string.app_name)
         setSupportActionBar(toolbar)
+
+        val bIr = findViewById<Button>(R.id.bIr)
+
+            bIr.setOnClickListener {
+                val intent = Intent(this, PantallaDos::class.java)
+                startActivity(intent)
+            }
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
