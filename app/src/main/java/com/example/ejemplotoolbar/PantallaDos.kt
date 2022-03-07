2fb4bb002709aad1d7bfd89490da4e3f4ab2791f
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-03-06 20:43:30 -0500

    PantallaDos

---
+++ app/src/main/java/com/example/ejemplotoolbar/PantallaDos.kt
@@ -0,0 +1,13 @@
+package com.example.ejemplotoolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaDos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_dos)
+
+
+    }
+}
