c6af77186487cc2c43496438e53a717411b48a97
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-03-06 21:50:27 -0500

    6

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -9,7 +9,9 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.ShareActionProvider
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.MenuItemCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +38,10 @@ class MainActivity : AppCompatActivity() {
 
         val itemsBusqueda = menu?.findItem(R.id.busqueda)
         val vistaBusqueda = itemsBusqueda?.actionView as SearchView
+        val itemCompartir = menu?.findItem(R.id.share)
+        val shareActionProvider = MenuItemCompat.getActionProvider(itemCompartir) as ShareActionProvider
+
+        compartirIntent(shareActionProvider)
 
         vistaBusqueda.queryHint = "Escribe tu nombre..."
 
@@ -70,5 +76,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun compartirIntent(shareActionProvider: ShareActionProvider) {
+        if(shareActionProvider != null) {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, "Este es un mensaje compartido")
+            shareActionProvider.setShareIntent(intent)
+        }
     }
 }
