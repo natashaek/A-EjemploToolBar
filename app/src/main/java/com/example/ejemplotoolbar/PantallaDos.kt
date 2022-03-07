0e55a125b83a4c67c7a5e566abe8900531f31510
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-03-06 20:47:53 -0500

    2

--- app/src/main/java/com/example/ejemplotoolbar/PantallaDos.kt
+++ app/src/main/java/com/example/ejemplotoolbar/PantallaDos.kt
@@ -2,12 +2,23 @@ package com.example.ejemplotoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 
 class PantallaDos : AppCompatActivity() {
+    var toolbar: Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_dos)
 
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_pantalla_dos, menu)
 
+        return super.onCreateOptionsMenu(menu)
     }
 }
